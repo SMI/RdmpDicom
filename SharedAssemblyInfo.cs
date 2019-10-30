e914695cfe6c063a90134df90e71d89f237fdeba
Thomas Nind <TZNind@dundee.ac.uk>
2019-10-30 14:53:50 +0000

    Bumped RDMP API and plugin version number

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -7,6 +7,6 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 
 // These should be replaced with correct values by the release process
-[assembly: AssemblyVersion("1.3.1")]
-[assembly: AssemblyFileVersion("1.3.1")]
-[assembly: AssemblyInformationalVersion("1.3.1")]
+[assembly: AssemblyVersion("1.3.2")]
+[assembly: AssemblyFileVersion("1.3.2")]
+[assembly: AssemblyInformationalVersion("1.3.2")]
