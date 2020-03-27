c20fa74a5e081f0b94ea8e8d544c2f91154ef9de
tznind <t.z.nind@dundee.ac.uk>
2020-03-27 08:10:10 +0000

    Removed substring in favour of more reliable/specific operation Trim

--- Rdmp.Dicom.Tests/NuspecIsCorrectTests.cs
+++ Rdmp.Dicom.Tests/NuspecIsCorrectTests.cs
@@ -64,9 +64,8 @@ namespace Rdmp.Dicom.Tests
                         string versionDependency = d.Groups[2].Value;
 
                         // NOTE(rkm 2020-02-14) Fix for specifiers which contain lower or upper bounds
-                        if (versionDependency.Contains("[") || versionDependency.Contains("("))
-                            versionDependency = versionDependency.Substring(1, versionDependency.Length - 2);
-
+                        versionDependency = versionDependency?.Trim('(',')','[',']');
+                        
                         if (packageDependency.Equals(package))
                         {
                             Assert.AreEqual(version, versionDependency, "Package {0} is version {1} in {2} but version {3} in {4}", package, version, csproj, versionDependency, nuspec);
