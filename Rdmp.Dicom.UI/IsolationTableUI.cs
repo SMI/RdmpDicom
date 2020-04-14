d7ea628cced1c35c498109f33710a0e4749e6be9
tznind <t.z.nind@dundee.ac.uk>
2020-04-14 09:36:50 +0100

    Added placeholder UI for isolation reviewer UI

---
+++ Rdmp.Dicom.UI/IsolationTableUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Forms;
+using Rdmp.Core.Curation;
+using Rdmp.Core.Curation.Data.DataLoad;
+using Rdmp.Core.DataLoad.Engine.LoadExecution.Components.Arguments;
+using Rdmp.Core.DataLoad.Engine.LoadExecution.Components.Runtime;
+using Rdmp.Core.Repositories;
+using Rdmp.Dicom.PipelineComponents;
+
+namespace Rdmp.Dicom.UI
+{
+    public partial class IsolationTableUI : Form
+    {
+        private readonly IsolationReview _reviewer;
+
+        public IsolationTableUI(IsolationReview reviewer)
+        {
+            _reviewer = reviewer;
+            InitializeComponent();
+        }
+    }
+}
---
+++ Rdmp.Dicom/PipelineComponents/IsolationReview.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommandLine;
+using Rdmp.Core.Curation.Data.DataLoad;
+
+namespace Rdmp.Dicom.PipelineComponents
+{
+    public class IsolationReview
+    {
+        public string Error { get; }
+
+        public IsolationReview(ProcessTask processTask)
+        {
+            if(processTask == null)
+                throw new ArgumentNullException(nameof(processTask));
+
+            if(!processTask.IsPluginType() || processTask.ProcessTaskType != ProcessTaskType.MutilateDataTable || processTask.Path != typeof(PrimaryKeyCollisionIsolationMutilation).FullName)
+                Error = "ProcessTask is not an isolation mutilation";
+
+
+            // TODO: get stuff to find tables etc
+            processTask.GetAllArguments();
+
+        }
+    }
+}
