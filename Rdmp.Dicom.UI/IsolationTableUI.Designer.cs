2015d82099aa062556c6fd79997e161b95e87815
tznind <t.z.nind@dundee.ac.uk>
2020-04-15 08:41:35 +0100

    Added UI elements

--- Rdmp.Dicom.UI/IsolationTableUI.Designer.cs
+++ Rdmp.Dicom.UI/IsolationTableUI.Designer.cs
@@ -32,12 +32,20 @@ namespace Rdmp.Dicom.UI
         {
             this.flpTables = new System.Windows.Forms.FlowLayoutPanel();
             this.label1 = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.tbTop = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.tbTimeout = new System.Windows.Forms.ToolStripTextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // flpTables
             // 
             this.flpTables.Dock = System.Windows.Forms.DockStyle.Top;
-            this.flpTables.Location = new System.Drawing.Point(0, 13);
+            this.flpTables.Location = new System.Drawing.Point(0, 38);
             this.flpTables.Name = "flpTables";
             this.flpTables.Size = new System.Drawing.Size(974, 30);
             this.flpTables.TabIndex = 0;
@@ -52,16 +60,70 @@ namespace Rdmp.Dicom.UI
             this.label1.TabIndex = 1;
             this.label1.Text = "Isolation Tables:";
             // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.tbTop,
+            this.toolStripLabel2,
+            this.tbTimeout});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 13);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(974, 25);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(31, 22);
+            this.toolStripLabel1.Text = "Top:";
+            // 
+            // tbTop
+            // 
+            this.tbTop.Name = "tbTop";
+            this.tbTop.Size = new System.Drawing.Size(100, 25);
+            this.tbTop.Click += new System.EventHandler(this.tbTop_Click);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(55, 22);
+            this.toolStripLabel2.Text = "Timeout:";
+            // 
+            // tbTimeout
+            // 
+            this.tbTimeout.Name = "tbTimeout";
+            this.tbTimeout.Size = new System.Drawing.Size(100, 25);
+            this.tbTimeout.Click += new System.EventHandler(this.tbTimeout_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 68);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(974, 604);
+            this.dataGridView1.TabIndex = 5;
+            // 
             // IsolationTableUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(974, 672);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.flpTables);
+            this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.label1);
             this.Name = "IsolationTableUI";
             this.Text = "Isolation Table Reviewer";
             this.Load += new System.EventHandler(this.IsolationTableUI_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -71,5 +133,11 @@ namespace Rdmp.Dicom.UI
 
         private System.Windows.Forms.FlowLayoutPanel flpTables;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox tbTop;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripTextBox tbTimeout;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- Rdmp.Dicom.UI/IsolationTableUI.cs
+++ Rdmp.Dicom.UI/IsolationTableUI.cs
@@ -1,12 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
 using FAnsi.Discovery;
-using Rdmp.Core.Curation;
 using Rdmp.Core.Curation.Data;
-using Rdmp.Core.Curation.Data.DataLoad;
-using Rdmp.Core.DataLoad.Engine.LoadExecution.Components.Arguments;
-using Rdmp.Core.DataLoad.Engine.LoadExecution.Components.Runtime;
-using Rdmp.Core.Repositories;
 using Rdmp.Dicom.PipelineComponents;
 
 namespace Rdmp.Dicom.UI
@@ -14,11 +13,15 @@ namespace Rdmp.Dicom.UI
     public partial class IsolationTableUI : Form
     {
         private readonly IsolationReview _reviewer;
+        private List<IsolationDifference> _currentDiffs;
+        private DataTable _currentDataTable;
 
         public IsolationTableUI(IsolationReview reviewer)
         {
             _reviewer = reviewer;
             InitializeComponent();
+
+            dataGridView1.CellFormatting += DataGridView1OnCellFormatting;
         }
 
         private void IsolationTableUI_Load(object sender, System.EventArgs e)
@@ -34,7 +37,55 @@ namespace Rdmp.Dicom.UI
 
         private void HandleClick(KeyValuePair<TableInfo, DiscoveredTable> kvp)
         {
+            dataGridView1.DataSource = _currentDataTable = _reviewer.GetDifferences(kvp, out _currentDiffs);
+        }
+        
+        private void DataGridView1OnCellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            var diff = _currentDiffs?.FirstOrDefault(d => d.RowIndex == e.RowIndex);
+
+            if (diff != null && !diff.IsMaster && _currentDataTable != null)
+            {
+                var colName = _currentDataTable.Columns[e.ColumnIndex].ColumnName;
+                
+                if(diff.ConflictingColumns.Contains(colName))
+                    e.CellStyle.BackColor = Color.LightCyan;
+            }
+        }
+
+        private void tbTimeout_Click(object sender, System.EventArgs e)
+        {
+            tbTimeout.ForeColor = Color.Black;
+
+            try
+            {
+                if (string.IsNullOrWhiteSpace(tbTimeout.TextBox.Text))
+                    _reviewer.Timeout = 0;
+                else
+                    _reviewer.Timeout = int.Parse(tbTimeout.TextBox.Text);
+            }
+            catch (Exception)
+            {
+                tbTimeout.ForeColor = Color.Red;
+            }
+        }
+
+        private void tbTop_Click(object sender, System.EventArgs e)
+        {
             
+            tbTop.ForeColor = Color.Black;
+
+            try
+            {
+                if (string.IsNullOrWhiteSpace(tbTop.TextBox.Text))
+                    _reviewer.Top = 0;
+                else
+                    _reviewer.Top = int.Parse(tbTop.TextBox.Text);
+            }
+            catch (Exception)
+            {
+                tbTop.ForeColor = Color.Red;
+            }
         }
     }
 }
