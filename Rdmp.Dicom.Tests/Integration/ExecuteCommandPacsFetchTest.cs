102efaebc7bac84e2213ed0ef5e31e2cf61f0c80
James A Sutherland <j.a.sutherland@dundee.ac.uk>
2021-05-26 16:33:30 +0000

    Use port 11112 for test PACS fetch in both directions

--- Rdmp.Dicom.Tests/Integration/ExecuteCommandPacsFetchTest.cs
+++ Rdmp.Dicom.Tests/Integration/ExecuteCommandPacsFetchTest.cs
@@ -12,7 +12,7 @@ namespace Rdmp.Dicom.Tests.Integration
         public void TestLocal()
         {
             
-            var cmd = new ExecuteCommandPacsFetch(new ConsoleInputManager(RepositoryLocator, new ThrowImmediatelyCheckNotifier()){DisallowInput= true},"2010-01-01","2020-01-01","www.dicomserver.co.uk",104,"you","localhost",23,"me",TestContext.CurrentContext.WorkDirectory,0);
+            var cmd = new ExecuteCommandPacsFetch(new ConsoleInputManager(RepositoryLocator, new ThrowImmediatelyCheckNotifier()){DisallowInput= true},"2010-01-01","2020-01-01","www.dicomserver.co.uk",11112,"you","localhost",11112,"me",TestContext.CurrentContext.WorkDirectory,0);
             cmd.Execute();
         }
     }
