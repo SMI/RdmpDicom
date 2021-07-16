04cf72c77c6f2dec62dfb7040f7129f9f1242678
Thomas Nind <TZNind@dundee.ac.uk>
2021-07-16 17:08:31 +0100

    Added association tracking and PressureGauge

---
+++ Rdmp.Dicom.Tests/PressureGaugeTests.cs
@@ -0,0 +1,84 @@
+﻿using NUnit.Framework;
+using Rdmp.Dicom.Cache.Pipeline;
+using ReusableLibraryCode.Progress;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rdmp.Dicom.Tests
+{
+    class PressureGaugeTests
+    {
+        [Test]
+        public void TestGuage_NotReached()
+        {
+            bool someFact = false;
+
+            var g = new PressureGauge();
+            g.ThresholdBeatsPerMinute = 4;
+            g.Tick(new DateTime(2001, 01, 01, 01, 01, 01), new ThrowImmediatelyDataLoadEventListener(), () => someFact = true);
+            Assert.IsFalse(someFact);
+        }
+        [Test]
+        public void TestGuage_NotReached_OverTime()
+        {
+            bool someFact = false;
+
+            var g = new PressureGauge();
+            g.ThresholdBeatsPerMinute = 1;
+
+            // events are 1 minute appart so does not trigger
+            g.Tick(new DateTime(2001, 01, 01, 01, 01, 01), new ThrowImmediatelyDataLoadEventListener(), () => someFact = true);
+            Assert.IsFalse(someFact);
+            g.Tick(new DateTime(2001, 01, 01, 01, 02, 01), new ThrowImmediatelyDataLoadEventListener(), () => someFact = true);
+            Assert.IsFalse(someFact);
+            g.Tick(new DateTime(2001, 01, 01, 01, 03, 01), new ThrowImmediatelyDataLoadEventListener(), () => someFact = true);
+            Assert.IsFalse(someFact);
+        }
+        [Test]
+        public void TestGuage_Reached()
+        {
+            bool someFact = false;
+
+            var g = new PressureGauge();
+            g.ThresholdBeatsPerMinute = 4;
+            g.Tick(new DateTime(2001, 01, 01, 01, 01, 01), new ThrowImmediatelyDataLoadEventListener(), () => someFact = true);
+            Assert.IsFalse(someFact);
+            g.Tick(new DateTime(2001, 01, 01, 01, 01, 01), new ThrowImmediatelyDataLoadEventListener(), () => someFact = true);
+            Assert.IsFalse(someFact);
+            g.Tick(new DateTime(2001, 01, 01, 01, 01, 01), new ThrowImmediatelyDataLoadEventListener(), () => someFact = true);
+            Assert.IsFalse(someFact);
+            g.Tick(new DateTime(2001, 01, 01, 01, 01, 01), new ThrowImmediatelyDataLoadEventListener(), () => someFact = true);
+            Assert.IsFalse(someFact);
+            g.Tick(new DateTime(2001, 01, 01, 01, 01, 01), new ThrowImmediatelyDataLoadEventListener(), () => someFact = true);
+            Assert.IsTrue(someFact);
+        }
+
+        [Test]
+        public void TestGuage_Reached_OverTime()
+        {
+            bool someFact = false;
+
+            var g = new PressureGauge();
+            g.ThresholdBeatsPerMinute = 1;
+            g.Tick(new DateTime(2001, 01, 01, 01, 01, 01), new ThrowImmediatelyDataLoadEventListener(), () => someFact = true);
+            Assert.IsFalse(someFact);
+            g.Tick(new DateTime(2001, 01, 01, 01, 01, 30), new ThrowImmediatelyDataLoadEventListener(), () => someFact = true);
+            Assert.IsTrue(someFact);
+        }
+        [Test]
+        public void TestGuage_Reached_OverTime_Boundary()
+        {
+            bool someFact = false;
+
+            var g = new PressureGauge();
+            g.ThresholdBeatsPerMinute = 1;
+            g.Tick(new DateTime(2001, 01, 01, 01, 01, 30), new ThrowImmediatelyDataLoadEventListener(), () => someFact = true);
+            Assert.IsFalse(someFact);
+            g.Tick(new DateTime(2001, 01, 01, 01, 02, 29), new ThrowImmediatelyDataLoadEventListener(), () => someFact = true);
+            Assert.IsTrue(someFact);
+        }
+    }
+}
