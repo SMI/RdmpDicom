3639ae812c2a658ccffa7ad9b68602dee4b6d2d5
James Friel <JFriel001@dundee.ac.uk>
2024-03-11 12:23:59 +0000

    merge

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCulture("")]
 
 // These should be replaced with correct values by the release process
-[assembly: AssemblyVersion("7.0.2")]
-[assembly: AssemblyFileVersion("7.0.2")]
-[assembly: AssemblyInformationalVersion("7.0.2")]
+[assembly: AssemblyVersion("7.0.3")]
+[assembly: AssemblyFileVersion("7.0.3")]
+[assembly: AssemblyInformationalVersion("7.0.3")]
 [assembly: InternalsVisibleTo("Rdmp.Dicom.Tests")]
