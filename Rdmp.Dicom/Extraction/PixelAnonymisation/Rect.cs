47f418a5c5fb8a8f255098b6e9e848ff3972d42e
James Friel <JFriel001@dundee.ac.uk>
2025-06-06 07:34:11 +0100

    add missing file

---
+++ Rdmp.Dicom/Extraction/PixelAnonymisation/Rect.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terminal.Gui.Trees;
+
+namespace Rdmp.Dicom.Extraction.PixelAnonymisation
+{
+    public class Rect
+    {
+        public int X1 { get; set; }
+        public int Y1 { get; set; }
+        public int X2 { get;  }
+        public int Y2 { get;  }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Rect(int x, int y, int width,int height)
+        {
+            X1 = x;
+            Y1 = y;
+            Width = width;
+            Height = height;
+            X2 = X1 + Width;
+            Y2 = Y1 + Height;
+        }
+    }
+}
