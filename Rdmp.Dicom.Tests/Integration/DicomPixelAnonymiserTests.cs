c4e4f749fe9c10828b48576be2fd1f1664f98bf5
James Friel <JFriel001@dundee.ac.uk>
2025-08-13 12:10:08 +0100

    remove test

--- Rdmp.Dicom.Tests/Integration/DicomPixelAnonymiserTests.cs
+++ Rdmp.Dicom.Tests/Integration/DicomPixelAnonymiserTests.cs
@@ -20,39 +20,39 @@ public class DicomPixelAnonymiserTests
 {
 
     [Test]
-    public void TestPixelAnonymiser()
-    {
-        var anonymiser = new DicomPixelAnonymiser();
-        anonymiser.RelativeArchiveColumnName = "dicomFile";
-        anonymiser.RemoveForms = true;
-        anonymiser.PutterType = typeof(PutInRoot);
-        anonymiser.PythonLocation = "C:\\Users\\jfriel001\\AppData\\Local\\Programs\\Python\\Python313\\Python313.dll";
-        //    anonymiser.ImagesAlreadyInDestination = true;
-        //    anonymiser.TesseractDataFolder = "C:\\temp\\tessdata";
-        anonymiser.Language = "en";
-        IExtractCommand cmd = MockExtractionCommand();
+    //public void TestPixelAnonymiser()
+    //{
+    //    var anonymiser = new DicomPixelAnonymiser();
+    //    anonymiser.RelativeArchiveColumnName = "dicomFile";
+    //    anonymiser.RemoveForms = true;
+    //    anonymiser.PutterType = typeof(PutInRoot);
+    //    anonymiser.PythonLocation = "C:\\Users\\jfriel001\\AppData\\Local\\Programs\\Python\\Python313\\Python313.dll";
+    //    //    anonymiser.ImagesAlreadyInDestination = true;
+    //    //    anonymiser.TesseractDataFolder = "C:\\temp\\tessdata";
+    //    anonymiser.Language = "en";
+    //    IExtractCommand cmd = MockExtractionCommand();
 
-        var source = new CancellationTokenSource();
-        //    //give the mock to anonymiser
-        anonymiser.PreInitialize(cmd, ThrowImmediatelyDataLoadEventListener.Quiet);
+    //    var source = new CancellationTokenSource();
+    //    //    //give the mock to anonymiser
+    //    anonymiser.PreInitialize(cmd, ThrowImmediatelyDataLoadEventListener.Quiet);
 
-        var dt = new DataTable();
-        dt.Columns.Add("Pat");
-        dt.Columns.Add("dicomFile");
-        //dt.Rows.Add(["1111111111", "C:\\temp\\dicoms\\002_with_ann.dcm\\dicom-00005.dcm"]);
-        dt.Rows.Add(["1111111111", "C:\\Users\\jfriel001\\Downloads\\dcmtest\\form_-dicom-00001.dcm"]);
-        //dt.Rows.Add(["1111111111", "C:\\temp\\dicoms\\002_with_ann.dcm\\dicom-00002.dcm"]);
-        //dt.Rows.Add(["1111111111", "C:\\temp\\dicoms\\002_with_ann.dcm\\dicom-00003.dcm"]);
-        //dt.Rows.Add(["1111111111", "C:\\temp\\dicoms\\002_with_ann.dcm\\dicom-00004.dcm"]);
-        //dt.Rows.Add(["1111111111", "C:\\Users\\jfriel001\\Downloads\\I290.dcm"]);
-        //dt.Rows.Add(["1111111111", "C:\\Users\\jfriel001\\Downloads\\sb.dcm"]);
-        //dt.Rows.Add(["1111111111", "C:\\Users\\jfriel001\\Downloads\\MF_-dicom-00001.dcm"]);
-        //dt.Rows.Add(["1111111111", "C:\\Users\\jfriel001\\Downloads\\ACUSON-24-YBR_FULL-RLE-b.dcm"]);
-        //dt.Rows.Add(["1111111111", "C:\\Users\\jfriel001\\Downloads\\form_example.png.dcm"]);
+    //    var dt = new DataTable();
+    //    dt.Columns.Add("Pat");
+    //    dt.Columns.Add("dicomFile");
+    //    //dt.Rows.Add(["1111111111", "C:\\temp\\dicoms\\002_with_ann.dcm\\dicom-00005.dcm"]);
+    //    dt.Rows.Add(["1111111111", "C:\\Users\\jfriel001\\Downloads\\dcmtest\\form_-dicom-00001.dcm"]);
+    //    //dt.Rows.Add(["1111111111", "C:\\temp\\dicoms\\002_with_ann.dcm\\dicom-00002.dcm"]);
+    //    //dt.Rows.Add(["1111111111", "C:\\temp\\dicoms\\002_with_ann.dcm\\dicom-00003.dcm"]);
+    //    //dt.Rows.Add(["1111111111", "C:\\temp\\dicoms\\002_with_ann.dcm\\dicom-00004.dcm"]);
+    //    //dt.Rows.Add(["1111111111", "C:\\Users\\jfriel001\\Downloads\\I290.dcm"]);
+    //    //dt.Rows.Add(["1111111111", "C:\\Users\\jfriel001\\Downloads\\sb.dcm"]);
+    //    //dt.Rows.Add(["1111111111", "C:\\Users\\jfriel001\\Downloads\\MF_-dicom-00001.dcm"]);
+    //    //dt.Rows.Add(["1111111111", "C:\\Users\\jfriel001\\Downloads\\ACUSON-24-YBR_FULL-RLE-b.dcm"]);
+    //    //dt.Rows.Add(["1111111111", "C:\\Users\\jfriel001\\Downloads\\form_example.png.dcm"]);
 
-        anonymiser.ProcessPipelineData(dt, ThrowImmediatelyDataLoadEventListener.Quiet, new());
-        //    Assert.That(1, Is.EqualTo(1));
-    }
+    //    anonymiser.ProcessPipelineData(dt, ThrowImmediatelyDataLoadEventListener.Quiet, new());
+    //    //    Assert.That(1, Is.EqualTo(1));
+    //}
 
     private static IExtractDatasetCommand MockExtractionCommand()
     {
